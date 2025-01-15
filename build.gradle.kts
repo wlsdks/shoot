2a4fb22c24b6472afb43f35c50002df72b38141b
wlsdks <wlsdks12@naver.com>
2025-01-15 21:21:45 +0900

    :sparkles: 초기 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,29 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	// Spring Boot
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+
+	// MongoDB
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+	// kotlin
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// Coroutines 지원
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
+	// logging
+	runtimeOnly("io.github.oshai:kotlin-logging:7.0.3")
+
+	// jsoup
+	implementation("org.jsoup:jsoup:1.18.3")
+
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")

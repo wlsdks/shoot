0e6a1e1f2f5c237e5cb0707315ad4569471f502d
wlsdks <wlsdks12@naver.com>
2025-01-19 14:42:32 +0900

    :recycle: 웹소켓 채팅 핸들러 코드 수정

--- src/main/kotlin/com/stark/shoot/adapter/in/websocket/ChatMessageHandler.kt
+++ src/main/kotlin/com/stark/shoot/adapter/in/websocket/ChatMessageHandler.kt
@@ -1,7 +1,7 @@
 package com.stark.shoot.adapter.`in`.websocket
 
 import com.stark.shoot.adapter.`in`.web.dto.ChatMessageRequest
-import com.stark.shoot.application.port.`in`.SendMessageUseCase
+import com.stark.shoot.application.port.`in`.chat.SendMessageUseCase
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Controller
@@ -24,26 +24,15 @@ class ChatMessageHandler(
         println("Received message: ${message.content}")
 
         val userId = principal.name // 현재 사용자 ID 가져오기
-//        if(!hasPermissionToSendMessage(userId, message.roomId)) {
-        // 권한이 없는 경우
-//            throw AccessDeniedException("You don't have permission to send message to this room.")
-//        }
 
-        // 메시지 저장
-        val savedMessage = sendMessageUseCase.sendMessage(
-            roomId = message.roomId,
-            senderId = message.senderId,
-            messageContent = message.toDomain()
+        // 1. kafka로 메시지 전송
+        sendMessageUseCase.handleMessage(message, userId)
+
+        // 2. 실시간으로 클라이언트에 전송
+        messagingTemplate.convertAndSend(
+            "/topic/messages/${message.roomId}",
+            message
         )
-
-        // 브로드캐스트
-        messagingTemplate.convertAndSend("/topic/messages/${message.roomId}", savedMessage)
     }
-
-//    private fun hasPermissionToSendMessage(userId: String, roomId: String): Boolean {
-//        val chatRoom = loadChatRoomPort.findById(roomId.toObjectId()) ?: return false
-//        return chatRoom.participants.contains(userId)
-//    }
-
 
 }

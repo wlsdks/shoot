4d0133e91d9585c68d7a0e8f2639c18e6c0a599a
wlsdks <wlsdks12@naver.com>
2025-02-09 02:53:19 +0900

    :spakles: 인증된 사용자 정보를 확인하는 api 추가

--- src/main/kotlin/com/stark/shoot/infrastructure/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/stark/shoot/infrastructure/config/security/SecurityConfig.kt
@@ -82,9 +82,9 @@ class SecurityConfig(
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authorizeHttpRequests {
                 // 인증이 필요하지 않은 경로
-                it.requestMatchers("/api/v1/users/**").permitAll() // 사용자 생성
-                it.requestMatchers("/api/v1/auth/**").permitAll()  // 로그인, 회원가입 허용
-                it.requestMatchers("/ws/**").permitAll()           // 웹소켓 핸드셰이크
+                // 로그인, 회원가입 등은 permitAll
+                it.requestMatchers("/api/v1/auth/login", "/api/v1/users").permitAll()
+                it.requestMatchers("/ws/**").permitAll() // 웹소켓 핸드셰이크
                 // 그 외 모든 요청은 인증 필요
                 it.anyRequest().authenticated()
             }

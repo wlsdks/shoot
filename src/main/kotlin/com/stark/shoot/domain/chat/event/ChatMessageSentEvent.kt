0b8f51af1242e3951a00be110a28d4bd3cec90ce
wlsdks <wlsdks12@naver.com>
2025-02-16 23:49:21 +0900

    :recycle: 사용하지 않게된 코드 제거

--- src/main/kotlin/com/stark/shoot/domain/chat/event/ChatMessageSentEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.stark.shoot.domain.chat.event
-
-import com.stark.shoot.domain.chat.message.ChatMessage
-import com.stark.shoot.domain.common.DomainEvent
-
-class ChatMessageSentEvent(
-    val chatMessage: ChatMessage
-) : DomainEvent

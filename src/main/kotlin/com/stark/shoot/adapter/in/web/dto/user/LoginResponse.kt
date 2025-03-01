11dad10a99ef68c6226ee874fd7b092938aa9a4f
wlsdks <wlsdks12@naver.com>
2025-03-01 22:25:27 +0900

    :sparkles: 로그인 응답객체에 refreshToken 추가

--- src/main/kotlin/com/stark/shoot/adapter/in/web/dto/user/LoginResponse.kt
+++ src/main/kotlin/com/stark/shoot/adapter/in/web/dto/user/LoginResponse.kt
@@ -2,5 +2,6 @@ package com.stark.shoot.adapter.`in`.web.dto.user
 
 data class LoginResponse(
     val userId: String,
-    val accessToken: String
+    val accessToken: String,
+    val refreshToken: String
 )

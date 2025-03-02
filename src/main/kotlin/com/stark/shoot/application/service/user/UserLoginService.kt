ab750805bbc5b859b3ed608e5165290f845be692
wlsdks <wlsdks12@naver.com>
2025-03-02 23:49:41 +0900

    :sparkles: 로그인할때 비밀번호 인증하는 로직 추가

--- src/main/kotlin/com/stark/shoot/application/service/user/UserLoginService.kt
+++ src/main/kotlin/com/stark/shoot/application/service/user/UserLoginService.kt
@@ -6,6 +6,7 @@ import com.stark.shoot.application.port.out.user.RetrieveUserPort
 import com.stark.shoot.application.port.out.user.token.RefreshTokenStorePort
 import com.stark.shoot.domain.chat.user.User
 import com.stark.shoot.infrastructure.config.jwt.JwtProvider
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service
 class UserLoginService(
     private val retrieveUserPort: RetrieveUserPort,
     private val refreshTokenStorePort: RefreshTokenStorePort,
+    private val passwordEncoder: PasswordEncoder,
     private val jwtProvider: JwtProvider,
 ) : UserLoginUseCase {
 
@@ -27,18 +29,21 @@ class UserLoginService(
         username: String,
         password: String
     ): LoginResponse {
+        // 사용자 조회
         val user: User = retrieveUserPort.findByUsername(username)
             ?: throw IllegalArgumentException("해당 username의 사용자를 찾을 수 없습니다.")
 
-        // 비밀번호 검증 (예시로 단순 비교, 실제로는 해시 검증 필요)
-        // if (user.password != password) throw IllegalArgumentException("비밀번호가 올바르지 않습니다.")
+        // 비밀번호 검증
+        if (!passwordEncoder.matches(password, user.passwordHash)) {
+            throw IllegalArgumentException("Invalid password")
+        }
 
+        // JWT 토큰 생성
         val accessToken = jwtProvider.generateToken(user.username, 60 * 1000) // 1분
         val refreshToken = jwtProvider.generateRefreshToken(user.username, 30 * 24 * 60) // 30일
 
         // MongoDB에 리프레시 토큰 저장
         refreshTokenStorePort.storeRefreshToken(user.id!!, refreshToken)
-
         return LoginResponse(user.id.toString(), accessToken, refreshToken)
     }
 

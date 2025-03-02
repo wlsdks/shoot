4dec8b8be7b76d7752e145daa04528692e40da05
wlsdks <wlsdks12@naver.com>
2025-03-02 23:46:14 +0900

    :recycle: 하나의 DTO파일 내부에 모든 Request, Response를 응집

--- src/main/kotlin/com/stark/shoot/adapter/in/web/dto/user/CreateUserRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.stark.shoot.adapter.`in`.web.dto.user
-
-data class CreateUserRequest(
-    val username: String,
-    val nickname: String
-)
--- src/main/kotlin/com/stark/shoot/adapter/in/web/dto/user/LoginResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.stark.shoot.adapter.`in`.web.dto.user
-
-data class LoginResponse(
-    val userId: String,
-    val accessToken: String,
-    val refreshToken: String
-)

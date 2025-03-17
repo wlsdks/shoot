e6307179b5169046bb78a1fe7637cd2e0cfcee3e
wlsdks <wlsdks12@naver.com>
2025-03-17 21:29:16 +0900

    :sparkles: CompletableFuture를 Coroutine으로 변환하는 함수 추가

---
+++ src/main/kotlin/com/stark/shoot/infrastructure/util/awaitResult.kt
@@ -0,0 +1,34 @@
+package com.stark.shoot.infrastructure.util
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.future.await
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.CompletableFuture
+
+/**
+ * CompletableFuture를 코루틴으로 변환하는 확장 함수
+ */
+suspend fun <T> CompletableFuture<T>.awaitResult(): T = withContext(Dispatchers.IO) {
+    this@awaitResult.await()
+}
+
+
+/**
+ * 코루틴 컨텍스트에서 CompletableFuture로 변환하는 함수
+ */
+fun <T> runAsync(block: suspend CoroutineScope.() -> T): CompletableFuture<T> {
+    val future = CompletableFuture<T>()
+
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val result = block()
+            future.complete(result)
+        } catch (e: Exception) {
+            future.completeExceptionally(e)
+        }
+    }
+
+    return future
+}

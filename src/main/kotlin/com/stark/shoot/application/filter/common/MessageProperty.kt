abd0a7a3eef04f77d356a339ec3c9d947e6376fb
wlsdks <wlsdks12@naver.com>
2025-04-05 21:23:34 +0900

    :recycle: 사용하지 않게된 코드 제거

--- src/main/kotlin/com/stark/shoot/application/filter/common/MessageProperty.kt
+++
@@ -1,9 +0,0 @@
-package com.stark.shoot.application.filter.common
-
-import com.stark.shoot.domain.chat.room.Participant
-import org.bson.types.ObjectId
-
-// 메시지 프로퍼티 (필터 간 데이터 전달용)
-data class MessageProperty(
-    val updatedParticipants: Map<ObjectId, Participant>
-)
--- src/main/kotlin/com/stark/shoot/application/port/out/user/token/RefreshTokenStorePort.kt
+++
@@ -1,6 +0,0 @@
-package com.stark.shoot.application.port.out.user.token
-
-interface RefreshTokenStorePort {
-    fun storeRefreshToken(userId: Long, refreshToken: String)
-    fun isValidRefreshToken(refreshToken: String): Boolean
-}

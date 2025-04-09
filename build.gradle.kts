5d63356017419ce26faf67a6d914557d23a30670
wlsdks <wlsdks12@naver.com>
2025-04-09 23:00:44 +0900

    :white_check_mark: fixtureMonkey 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.1.11")
 
     // mockito 오류 해결 (java 21)
     testImplementation("org.mockito:mockito-core:5.15.2")
--- src/test/kotlin/com/stark/shoot/ShootApplicationTests.kt
+++ src/test/kotlin/com/stark/shoot/ShootApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class ShootApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }

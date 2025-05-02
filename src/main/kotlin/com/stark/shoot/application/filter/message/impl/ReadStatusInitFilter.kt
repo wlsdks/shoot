c45b021b37b4df518740ae9e708b2ab65c7f5731
wlsdks <wlsdks12@naver.com>
2025-05-02 11:26:25 +0900

    :recycle: 읽음상태 변경 필터에 로직 추가

--- src/main/kotlin/com/stark/shoot/application/filter/message/impl/ReadStatusInitFilter.kt
+++ src/main/kotlin/com/stark/shoot/application/filter/message/impl/ReadStatusInitFilter.kt
@@ -23,14 +23,21 @@ class ReadStatusInitFilter(
         val chatRoom = chain.getFromContext<ChatRoom>(CHAT_ROOM_CONTEXT_KEY)
             ?: return chain.proceed(message) // 채팅방 정보가 없으면 다음 필터로 진행
 
+        // 메시지 발신자를 readBy 맵에 추가 (발신자는 자신의 메시지를 읽은 것으로 처리)
+        val updatedMessage = if (message.readBy[message.senderId] != true) {
+            message.markAsRead(message.senderId)
+        } else {
+            message
+        }
+
         // 메시지 처리 진행
-        val processedMessage = chain.proceed(message)
+        val processedMessage = chain.proceed(updatedMessage)
 
         // 발신자의 마지막 읽은 메시지 ID 업데이트
         if (processedMessage.id != null) {
             readStatusPort.updateLastReadMessageId(
-                roomId = message.roomId,
-                userId = message.senderId,
+                roomId = processedMessage.roomId,
+                userId = processedMessage.senderId,
                 messageId = processedMessage.id.toString()
             )
         }

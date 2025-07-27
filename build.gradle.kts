094c53177799fa869497c79ba797d5364bb54016
wlsdks <wlsdks12@naver.com>
2025-07-27 21:50:12 +0900

    :sparkles: 스프링 부트 버전을 lts인 3.5.x로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.4.3"
+    id("org.springframework.boot") version "3.5.4"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "1.9.25"
 }
@@ -58,7 +58,7 @@ dependencies {
     implementation("org.jsoup:jsoup:1.19.1")
 
     // swagger
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.3")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")

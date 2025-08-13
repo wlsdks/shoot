c2d3e79ebeff9c8863c9a5a970d7d5acb4306276
stark <wlsdks12@naver.com>
2025-08-13 11:37:17 +0900

    🐛 DomainConstants @ConstructorBinding 제거로 애플리케이션 시작 오류 수정
    - Spring Boot에서 data class 단일 생성자는 자동 바인딩됨
    - @ConstructorBinding이 불필요하게 추가되어 발생한 IllegalStateException 해결
    - ConfigurationProperties 바인딩 정상화

--- src/main/kotlin/com/stark/shoot/infrastructure/config/domain/DomainConstants.kt
+++ src/main/kotlin/com/stark/shoot/infrastructure/config/domain/DomainConstants.kt
@@ -1,13 +1,12 @@
 package com.stark.shoot.infrastructure.config.domain
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.bind.ConstructorBinding
 
 /**
  * 도메인 전반에서 사용되는 상수값들을 설정으로 관리
  */
 @ConfigurationProperties(prefix = "app.domain")
-data class DomainConstants @ConstructorBinding constructor(
+data class DomainConstants(
     val chatRoom: ChatRoomConstants = ChatRoomConstants(),
     val message: MessageConstants = MessageConstants(),
     val friend: FriendConstants = FriendConstants()
